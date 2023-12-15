0280476798676e90c01656a8dd44428a5b9d85d4
Govardhan GR <govardhan.gr@v-key.com>
2023-12-15 11:23:04 +0800

    Setup the data layer

---
+++ app/src/main/java/com/govardhan/newsapp/data/api/NetworkService.kt
@@ -0,0 +1,17 @@
+package com.govardhan.newsapp.data.api
+
+import com.govardhan.newsapp.data.model.TopHeadlinesResponse
+import com.govardhan.newsapp.utils.AppConstant.API_KEY
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+import javax.inject.Singleton
+
+@Singleton
+interface NetworkService {
+
+    @Headers("X-Api-Key: $API_KEY")
+    @GET("top-headlines")
+    suspend fun getTopHeadlines(@Query("country") country: String): TopHeadlinesResponse
+
+}
